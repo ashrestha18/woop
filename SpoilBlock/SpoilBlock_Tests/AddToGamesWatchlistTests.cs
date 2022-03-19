70bc5d9c3b999a998075e5c1f97825ec99cdf8a7
Ankit Shrestha <ashrestha18@mail.wou.edu>
2022-03-18 22:34:39 -0700

    Added test stubs for WP-50

--- SpoilBlock/SpoilBlock_Tests/AddToGamesWatchlistTests.cs
+++ SpoilBlock/SpoilBlock_Tests/AddToGamesWatchlistTests.cs
@@ -3,10 +3,62 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using Moq;
+using SpoilBlock.DAL.Abstract;
+using SpoilBlock.DAL.Concrete;
+using SpoilBlock.Models;
+using System.Net.Http;
+using Moq.Contrib.HttpClient;
 
 namespace SpoilBlock_Tests
 {
-    internal class AddToGamesWatchlistTests
+    public class AddToGamesWatchlistTests
     {
+
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void AddToGamesWatchlist_Should_AddOneResult()
+        {
+            //Arrange
+            var GameResults = new AddToGamesWatchlistService(new Mock<WOOPDbContext>.Object);
+
+            //Act
+            var actual = GameResults.AddToWatchlist().ToList();
+            var expected = new List<Games>();
+
+            // Assert that the added result, actual[1] == expected[1].
+            
+        }
+
+        [Test]
+        public void AddToGamesWatchlist_Should_AddMultipleResult()
+        {
+            //Arrange
+            var GameResults = new AddToGamesWatchlistService(new Mock<WOOPDbContext>.Object);
+
+            //Act
+            var actual = GameResults.AddToWatchlist().ToList();
+            var expected = new List<Games>();
+
+            //Assert that the added result, All element are actual == expected.
+
+        }
+
+        [Test]
+        public void AddToGamesWatchlist_Should_AddNoResult()
+        {
+            //Arrange
+            var GameResults = new AddToGamesWatchlistService(new Mock<WOOPDbContext>.Object);
+
+            //Act
+
+            //Assert  a message that "Nothing is added to watchlist"
+        }
     }
 }
--- SpoilBlock/SpoilBlock_Tests/RAWGGameSearchServiceTests.cs
+++ SpoilBlock/SpoilBlock_Tests/RAWGGameSearchServiceTests.cs
@@ -3,10 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using Moq;
+using SpoilBlock.DAL.Abstract;
+using SpoilBlock.DAL.Concrete;
+using SpoilBlock.Models;
+using System.Net.Http;
+using Moq.Contrib.HttpClient;
 
 namespace SpoilBlock_Tests
 {
-    internal class RAWGGameSearchService
+    public  class RAWGGameSearchServiceTests
     {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void RAWGGameSearchService_Should_ReturnEmptyListIfNoResult()
+        {
+            //Arrange
+            RAWGGameSearchService service = new RAWGGameSearchService(new Mock<IHttpClientFactory>().Object, new APIKeyAccessor(""));
+
+            //Act
+            List<GameEntry> actual = service.ParseRAWGJSON().ToList();
+
+            //Assert that GameEntry list count is 0 i.e; it is empty.
+        }
+
+        public void RAWGGameSearchService_Should_ReturnCorrectlyIntepretJSON()
+        {
+            //Arrange
+            RAWGGameSearchService service = new RAWGGameSearchService(new Mock<IHttpClientFactory>().Object, new APIKeyAccessor(""));
+
+            //Act
+            List<GameEntry> expected = service.ParseRAWGJSON().ToList();
+
+            //Assert that actual list is equal to the expected fake list
+        }
+
+        public void RAWGGameSearchService_Should_ThrowNullArgumentException_IfJSONIsNull()
+        {
+            //Arrange
+            RAWGGameSearchService service = new RAWGGameSearchService(new Mock<IHttpClientFactory>().Object, new APIKeyAccessor(""));
+            
+
+            //Assert throw NullArgumentException when JSON is null
+        }
     }
-}
